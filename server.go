a893f925dc8941e5ac7151b3a1f8f7d7b0830031
baremy <remy.bamas@ynov.com>
2023-02-03 11:11:21 +0100

    json functionnal

--- server.go
+++ server.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type artists struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Location     string   `json:"location"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+var artistsData []artists
+
 func main() {
-	fmt.Printf("http://localhost:8080")
-	url := "https://groupietrackers.herokuapp.com/api"
+	fmt.Println("http://localhost:8080")
+	url := "https://groupietrackers.herokuapp.com/api/artists"
 	req, _ := http.NewRequest("GET", url, nil)
 	res, _ := http.DefaultClient.Do(req)
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(body))
+	//fmt.Println(string(body))
+	err := json.Unmarshal([]byte(body), &artistsData)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	fmt.Println(artistsData)
+}
 
+func server() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello!")
 	})
