5265c0d13f1e11de9fb3d79949d42bd5bfb2fe02
fpierrea <pierrealexis.favril@ynov.com>
2023-02-03 09:30:22 +0100

    mise en place API

--- server.go
+++ server.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Printf("http://localhost:8080")
+	url := "https://groupietrackers.herokuapp.com/api"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello!")
@@ -16,5 +23,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
