1543d65cb94eb2c20ef87a592b31bcf036c3bbf5
baremy <remy.bamas@ynov.com>
2023-02-10 08:09:33 +0100

    css functionnal

--- server.go
+++ server.go
@@ -55,8 +55,8 @@ func artistHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("http://localhost:8080")
 	artist()
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", artistHandler)
-
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
