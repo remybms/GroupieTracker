fa84ab304efe5399970fe3a9fa7b4df5a0fcb0a1
fpierrea <pierrealexis.favril@ynov.com>
2023-02-19 18:51:01 +0100

    resolve js bug

--- static/html/animation.js
+++ static/html/animation.js
@@ -12,17 +12,13 @@ searchinput.AddEventListener('keyup', myResearch)
 function myResearch(){
     const input = searchinput.value;
     console.log(input);
-
     const result = artist.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
-
     let final = '';
-
     if (input != ""){
         result.forEach(resultItem =>
                 console.log(resultItem.name),
                 final += `<div class="suggestion">${resultItem.name}</div>`
             )
     }
-    
     document.getElementById("suggestion").innerHTML = final;
 }
