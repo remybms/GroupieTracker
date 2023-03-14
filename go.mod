5833e76fd372da6a293871e439d03095fcaf6381
baremy <remy.bamas@ynov.com>
2023-03-14 10:20:54 +0100

    get deezer artist id

--- go.mod
+++
@@ -1,5 +0,0 @@
-module spotify
-
-go 1.19
-
-require github.com/remybms/GroupieTracker v0.0.0-20230313081258-3ad965ac80ed // indirect
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
@@ -20,11 +21,20 @@ type artists struct {
 	Location     string   `json:"location"`
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
+	idDeezer     int
 }
 
 type relation struct {
 	Id             int
 	DatesLocations map[string][]string
+}
+
+type Deezer struct {
+	Id int
+}
+
+type extractDeezer struct {
+	Data []Deezer
 }
 
 type locations struct {
@@ -71,6 +81,7 @@ type concerts struct {
 
 var artistsData artistsArray
 var concertsData concerts
+var extractDeezerId extractDeezer
 
 func Artists() {
 
@@ -80,7 +91,6 @@ func Artists() {
 	body, _ := ioutil.ReadAll(res.Body)
 	//fmt.Println(string(body))
 	err := json.Unmarshal([]byte(body), &artistsData.Array)
-
 	if err != nil {
 		fmt.Println("Error :", err)
 		return
@@ -198,6 +208,22 @@ func handleNextButton(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, artistsDataPaginate)
 }
 
+func deezer() {
+	url := fmt.Sprintf("https://api.deezer.com/search/artist/?q=%s&index=0&limit=2&output=json", artistsData.Array[index-1].Name)
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	body, _ := ioutil.ReadAll(res.Body)
+	//fmt.Println(string(body))
+	err := json.Unmarshal([]byte(body), &extractDeezerId)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	artistsData.Array[index-1].idDeezer = extractDeezerId.Data[0].Id
+	fmt.Println(artistsData.Array[index-1].idDeezer)
+	defer res.Body.Close()
+}
+
 func handlePrevButton(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./static/html/Home.html")
 	if err != nil {
@@ -249,205 +275,22 @@ func concertHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+var index int
+
 func artistHandler(w http.ResponseWriter, r *http.Request) {
 	indexString := r.FormValue("card")
-	index, _ := strconv.Atoi(indexString)
+	index, _ = strconv.Atoi(indexString)
 	t, err := template.ParseFiles("./static/html/Artist.html")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	deezer()
 	t.Execute(w, artistsData.Array[index-1])
 
 }
 
-// API Spotify//
-/*
-const (
-	clientID     = "b060eb29f3d44f388961e66fd7b55fa4"
-	clientSecret = "4bd35b387c0446aab01c3c4961c1439d"
-	redirectURI  = "http://localhost:8080/callback"
-)
-
-type Token struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int    `json:"expires_in"`
-	RefreshToken string `json:"refresh_token"`
-	Scope        string `json:"scope"`
-}
-
-type Artist struct {
-	ID         string   `json:"id"`
-	Name       string   `json:"name"`
-	Genres     []string `json:"genres"`
-	Followers  int      `json:"followers"`
-	Popularity int      `json:"popularity"`
-}
-
-type Track struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	PreviewURL string `json:"preview_url"`
-}
-
-type SearchResponse struct {
-	Artists []Artist `json:"artists"`
-	Tracks  []Track  `json:"tracks"`
-}
-
-func spotifyHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("./testspotify.html")
-
-	authURL := fmt.Sprintf("https://accounts.spotify.com/authorize?client_id=%s&response_type=code&redirect_uri=%s&scope=user-read-private+user-read-email+playlist-read-private", clientID, redirectURI)
-	http.Redirect(w, r, authURL, http.StatusFound)
-	t.Execute(w, http.StatusFound)
-}
-
-func getAccessToken(code string) (*Token, error) {
-	endpoint := fmt.Sprintf("https://accounts.spotify.com/api/token?grant_type=authorization_code&code=%s&redirect_uri=%s", code, redirectURI)
-
-	req, err := http.NewRequest("POST", endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.SetBasicAuth(clientID, clientSecret)
-
-	client := http.DefaultClient
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var token Token
-	err = json.Unmarshal(body, &token)
-	if err != nil {
-		return nil, err
-	}
-
-	return &token, nil
-}
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	code := r.URL.Query().Get("code")
-	if code == "" {
-		http.Error(w, "Failed to get authorization code", http.StatusBadRequest)
-		return
-	}
-
-	token, err := getAccessToken(code)
-	if err != nil {
-		http.Error(w, "Failed to get access token", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Access token: %s\n", token.AccessToken)
-
-	artist := r.URL.Query().Get("artist")
-	if artist == "" {
-		fmt.Fprintln(w, "Please provide an artist name")
-		return
-	}
-
-	searchResult, err := searchArtist(token.AccessToken, artist)
-	if err != nil {
-		http.Error(w, "Failed to search for artist", http.StatusInternalServerError)
-		return
-	}
-
-	if len(searchResult.Artists) == 0 {
-		fmt.Fprintf(w, "No artists found for \"%s\"\n", artist)
-		return
-	}
-
-	topTracks, err := getArtistTopTracks(token.AccessToken, searchResult.Artists[0].ID)
-	if err != nil {
-		http.Error(w, "Failed to get artist's top tracks", http.StatusInternalServerError)
-		return
-	}
-
-	if len(topTracks) == 0 {
-		fmt.Fprintf(w, "No top tracks found for \"%s\"\n", searchResult.Artists[0].Name)
-		return
-	}
-
-	fmt.Fprintf(w, "Playing preview of \"%s\" by \"%s\"\n", topTracks[0].Name, searchResult.Artists[0].Name)
-	fmt.Fprintf(w, "Preview URL: %s\n", topTracks[0])
-}
-
-func getArtistTopTracks(accessToken string, artistID string) ([]Track, error) {
-	endpoint := fmt.Sprintf("https://api.spotify.com/v1/artists/%s/top-tracks?market=US", artistID)
-
-	req, err := http.NewRequest("GET", endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	client := http.DefaultClient
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var topTracksResponse struct {
-		Tracks []Track `json:"tracks"`
-	}
-
-	err = json.Unmarshal(body, &topTracksResponse)
-	if err != nil {
-		return nil, err
-	}
-
-	return topTracksResponse.Tracks, nil
-}
-func searchArtist(accessToken string, artistName string) (*SearchResponse, error) {
-	endpoint := fmt.Sprintf("https://api.spotify.com/v1/search?q=%s&type=artist", url.QueryEscape(artistName))
-
-	req, err := http.NewRequest("GET", endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	client := http.DefaultClient
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var searchResult SearchResponse
-	err = json.Unmarshal(body, &searchResult)
-	if err != nil {
-		return nil, err
-	}
-
-	return &searchResult, nil
-}
-*/
-//API Spotify//
-
-/*func main() {
+func main() {
 	fmt.Println("http://localhost:8080")
 	feedData()
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -461,4 +304,4 @@ func searchArtist(accessToken string, artistName string) (*SearchResponse, error
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-}*/
+}
